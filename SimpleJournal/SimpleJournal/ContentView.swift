4329ff54e5ec840e9dca782ab6de1d073cb438a6
ttaww <101066449+ttaww@users.noreply.github.com>
2022-03-06 12:38:43 -0800

    Init commit

---
+++ SimpleJournal/SimpleJournal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleJournal
+//
+//  Created by WEI XIE on 2022-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleJournal/SimpleJournal/SimpleJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleJournalApp.swift
+//  SimpleJournal
+//
+//  Created by WEI XIE on 2022-03-06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
