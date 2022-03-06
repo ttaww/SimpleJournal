33a1fa6bfbc0d8c3fb2f6c42b25a4e7c6578e053
ttaww <101066449+ttaww@users.noreply.github.com>
2022-03-06 12:58:06 -0800

    Import coredata class

--- SimpleJournal/SimpleJournal/SimpleJournalApp.swift
+++ SimpleJournal/SimpleJournal/SimpleJournalApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct SimpleJournalApp: App {
+    
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
