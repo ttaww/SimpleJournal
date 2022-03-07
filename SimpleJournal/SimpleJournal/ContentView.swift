62402c0e85b1d94d59c2f2f55cdb355b4ef9c1cf
ttaww <101066449+ttaww@users.noreply.github.com>
2022-03-06 22:29:13 -0800

    Added navigation feature to the plus button

--- SimpleJournal/SimpleJournal/ContentView.swift
+++ SimpleJournal/SimpleJournal/ContentView.swift
@@ -9,19 +9,20 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @State var selection: Int? = nil
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        EntryDetailView(item: item)
                     } label: {
                         EntryCell(item: item)
                     }
@@ -33,21 +34,28 @@ struct ContentView: View {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    NavigationLink(destination:EntryDetailView(item: nil),
+                                   tag: 1,
+                                   selection: $selection){
+                        Button(action: {
+                            self.selection = 1
+                        }) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
+                
             }
             Text("Select an item")
         }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
             newItem.content = "Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String Temp String "
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -58,11 +66,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
--- SimpleJournal/SimpleJournal/EntryDetailView.swift
+++ SimpleJournal/SimpleJournal/EntryDetailView.swift
@@ -10,7 +10,7 @@ import CoreData
 
 
 struct EntryDetailView: View {
-    var item: Item
+    var item: Item?
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
